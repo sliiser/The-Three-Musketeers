92d9dbab663fc19a6105261e800be3a082d11c0d
Siim Liiser <siimliiser@gmail.com>
2014-11-10 17:03:28 +0200

    Added new constructor

--- trunk/src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
+++ trunk/src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
@@ -1,47 +1,56 @@
 package ee.ut.math.tvt.salessystem.domain.data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-
-
 /**
- * Already bought HistoryItem. stockItem duplicates name and price for preserving history. 
+ * Already bought HistoryItem. stockItem duplicates name and price for
+ * preserving history.
  */
 public class HistoryItem implements Cloneable, DisplayableItem {
-    
-    private String date;
-    private String time;
-    private double total;
-    private List<SoldItem> items;
-    
-    public HistoryItem(String date, String time, List<SoldItem> items) {
-        this.date = date;
-        this.time = time;
-        this.items = items;
-        double total = 0;
+
+	private String date;
+	private String time;
+	private double total;
+	private List<SoldItem> items;
+
+	public HistoryItem(List<SoldItem> items) {
+		this(new SimpleDateFormat("yyyy/MM/dd").format(new Date()),
+				new SimpleDateFormat("HH:mm:ss").format(new Date()), items);
+	}
+
+	public HistoryItem(String date, String time, List<SoldItem> items) {
+		this.date = date;
+		this.time = time;
+		this.items = items;
+		double total = 0;
 		for (final SoldItem item : items) {
 			total += item.getSum();
 		}
-        this.total = total;
-    }
+		this.total = total;
+	}
 
-    public String getDate() {
-    	return date;
-    }
-    public String getTime() {
-    	return time;
-    }
-    public double getTotal() {
-    	return total;
-    }
-    public List<SoldItem> getItems() {
-    	return items;
-    }
+	public String getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public List<SoldItem> getItems() {
+		return items;
+	}
 
 	@Override
 	public Long getId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-    
+
 }
